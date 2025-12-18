5367c3bd782100dc8ddd5f9c380dc321578cdc3a
tih-rushikeshbhomale <rushikesh.bhomale@tihiitb.org>
2025-12-18 14:12:11 +0530

    feat: Add Divide function

--- calculator.py
+++ calculator.py
@@ -4,12 +4,18 @@
 def subtract(x, y):
     return x - y
 
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+
 def main():
     print("Simple Calculator")
     print("1. Add")
     print("2. Subtract")
+    print("3. Divide")
     
-    choice = input("Enter choice(1/2): ")
+    choice = input("Enter choice(1/2/3): ")
     
     if choice == '1':
         num1 = float(input("Enter first number: "))
@@ -19,6 +25,10 @@ def main():
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         print(f"{num1} - {num2} = {subtract(num1, num2)}")
+    elif choice == '3':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(f"{num1} / {num2} = {divide(num1, num2)}")
     else:
         print("Invalid Input")
 
