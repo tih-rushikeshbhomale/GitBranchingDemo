251bf189badc9ba8c3c4d76e4700d3d69791481c
tih-rushikeshbhomale <rushikesh.bhomale@tihiitb.org>
2025-12-18 14:12:09 +0530

    feat: Add Multiply function

--- calculator.py
+++ calculator.py
@@ -4,12 +4,16 @@
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
 def main():
     print("Simple Calculator")
     print("1. Add")
     print("2. Subtract")
+    print("3. Multiply")
     
-    choice = input("Enter choice(1/2): ")
+    choice = input("Enter choice(1/2/3): ")
     
     if choice == '1':
         num1 = float(input("Enter first number: "))
@@ -19,6 +23,10 @@ def main():
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         print(f"{num1} - {num2} = {subtract(num1, num2)}")
+    elif choice == '3':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
     else:
         print("Invalid Input")
 
