810d9d4eeee5523d47047335a93b3162ae791728
tih-rushikeshbhomale <rushikesh.bhomale@tihiitb.org>
2025-12-18 14:12:09 +0530

    feat: Add Subtract function

--- calculator.py
+++ calculator.py
@@ -1,16 +1,24 @@
 ﻿def add(x, y):
     return x + y
 
+def subtract(x, y):
+    return x - y
+
 def main():
     print("Simple Calculator")
     print("1. Add")
+    print("2. Subtract")
     
-    choice = input("Enter choice(1): ")
+    choice = input("Enter choice(1/2): ")
     
     if choice == '1':
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         print(f"{num1} + {num2} = {add(num1, num2)}")
+    elif choice == '2':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(f"{num1} - {num2} = {subtract(num1, num2)}")
     else:
         print("Invalid Input")
 
